50657ad5bb283f0f7c21b06d8ceec2d6a8950bd1
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2018-10-27 13:01:02 -0500

    Working

--- backend/database-services/models/order.js
+++ backend/database-services/models/order.js
@@ -2,7 +2,7 @@ var {mongoose} = require('../dl-mongo');
 
 var schema = new mongoose.Schema({
     "jobId": { type: String, required: true },
-    "projectName": { type: String, required: true },
+    "projectName": { type: String },
     "status": {type: String, required: true },
     "createdDate": {type: Date, required: true },
     "assignedNode": {type: String }
