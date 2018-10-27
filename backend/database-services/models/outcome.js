a193842dc9d0bd4408c7abe06714a7b33f733b65
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2018-10-27 14:33:13 -0500

    Bugging

--- backend/database-services/models/outcome.js
+++ backend/database-services/models/outcome.js
@@ -2,7 +2,7 @@ var {mongoose} = require('../dl-mongo');
 
 var schema = new mongoose.Schema({
     "workerNodeName": String,
-    "status": {type: Number, required: true },
+    "status": {type: String, required: true },
     "orderId": {type: String, required: true },
     "loss": Number,
     "metric": Number
