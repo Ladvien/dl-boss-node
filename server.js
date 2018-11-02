7271186bd71e6e26e78a40eeac9178167ccada27
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2018-11-02 06:41:20 -0500

    Working

--- server.js
+++ server.js
@@ -169,7 +169,7 @@ app.get('/retrieve/:type/:id?/:param1?', (req, res) => {
                 });
                 break;
             case 'recent-job':
-                Job.findOne({}, { sort: {$natural:-1} })
+                Job.find().limit(1).sort({$natural:-1})
                 .then((response) => {
                     res.send(response);
                 });
