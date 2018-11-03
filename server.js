84447343c7ca5f12f3a7519eafbbe18badb35166
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2018-11-03 10:01:18 -0500

    Bugging /bored/id

--- server.js
+++ server.js
@@ -128,7 +128,8 @@ app.post('/bored/:id', (req, res) => {
         let workerNodeId = req.params.id;
         console.log(`${workerNodeId} said it's bored.`);
         if (!workerNodeId) { throw {'error': 'No id provided.'}}
-        Order.findOne({ status: 'unassigned' }, {}, { sort: { 'created_at' : -1 } }, (err, order) => {
+        Order.findOne({ status: 'unassigned' }, {}, { sort: { 'created_at' : -1 } })
+        .then((err, order) => {
             if (!order) throw Error({'error': 'No unassigned orders found.'});
             console.log(`Found a work order, #${order._id}`)
             // order.status = 'assigned';
